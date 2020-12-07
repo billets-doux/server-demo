c56d68f780f8ba3160d10ec08c47938a9d35ac75
billetsdoux <billetsdoux@github.com>
2020-12-08 02:06:54 +0800

    配置服务器

---
+++ server-common/src/main/java/com/billetsdoux/servercommon/ServerCommonApplication.java
@@ -0,0 +1,12 @@
+package com.billetsdoux.servercommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerCommonApplication.class, args);
+
+    }
+}
